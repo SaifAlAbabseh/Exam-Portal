31aaa482b268b949f9ab5ca1b0724723bef27ef3
Saif AlAbabseh <sababseh97@gmail.com>
2023-01-15 10:09:48 +0300

    Upload all project files.

--- Admin Portal/DBInfo.java
+++
@@ -1,7 +0,0 @@
-package examportal;
-
-public class DBInfo {
-	public static final String host="jdbc:mysql://localhost/examportal";
-	public static final String username="root";
-	public static final String password="12345678";
-}
